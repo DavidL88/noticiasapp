17fcaf9c01340de258b673ea4422199463b1155f
Nek <trunks_17_54@hotmail.com>
2024-01-23 12:20:09 -0600

    Add Formulario component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Container, Grid, Typography } from '@mui/material';
+import Formulario from './components/Formulario';
 
 function App() {
 
@@ -9,6 +10,18 @@ function App() {
         Buscador de Noticias
       </Typography>
     </header>
+
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid item xs={12} md={6} lg={4}>
+        <Formulario />  
+      </Grid>
+    </Grid>
+
    </Container>
   )
 }
